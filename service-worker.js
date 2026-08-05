a55724c87f30693001a9725f42ffea2deeb48de3
nydiabuilds <nydia.burgos@yahoo.com>
2026-08-04 22:57:31 -0400

    Add membership and Tell Tro experience

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE = "sourcetro-v5-smart-source-scan";
+const CACHE = "sourcetro-v6-membership-tell-tro";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=5",
-  "./app.js?v=5",
+  "./styles.css?v=6",
+  "./app.js?v=6",
   "./manifest.webmanifest",
   "./assets/sourcetro-mark.svg",
 ];
