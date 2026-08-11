420dd3bb5eec6a37b2f4fdd411da06122c0f7652
nydiabuilds <nydia.burgos@yahoo.com>
2026-08-10 20:48:47 -0400

    Hydrate trusted SourceTro device before integrations load

---
+++ trusted-session.js
@@ -0,0 +1,10 @@
+(() => {
+  const SESSION_OWNER_KEY = "sourcetro_owner_key";
+  const TRUSTED_OWNER_KEY = "sourcetro_trusted_owner_key";
+
+  try {
+    const trusted = localStorage.getItem(TRUSTED_OWNER_KEY) || "";
+    const session = sessionStorage.getItem(SESSION_OWNER_KEY) || "";
+    if (trusted && !session) sessionStorage.setItem(SESSION_OWNER_KEY, trusted);
+  } catch {}
+})();
