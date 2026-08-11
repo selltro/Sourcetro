aaa4f47e334454ea1b211401697e2c159d2bd82b
nydiabuilds <nydia.burgos@yahoo.com>
2026-08-11 04:10:44 -0400

    Fix trusted session so latest SourceTro build can load

--- trusted-session.js
+++ trusted-session.js
@@ -1,5 +1,4 @@
 (() => {
-  const BUILD = "40";
   const SESSION_OWNER_KEY = "sourcetro_owner_key";
   const TRUSTED_OWNER_KEY = "sourcetro_trusted_owner_key";
 
@@ -8,13 +7,4 @@
     const session = sessionStorage.getItem(SESSION_OWNER_KEY) || "";
     if (trusted && !session) sessionStorage.setItem(SESSION_OWNER_KEY, trusted);
   } catch {}
-
-  if ("serviceWorker" in navigator && typeof navigator.serviceWorker.register === "function") {
-    const originalRegister = navigator.serviceWorker.register.bind(navigator.serviceWorker);
-    navigator.serviceWorker.register = (url, options) => {
-      const requested = String(url || "");
-      const nextUrl = requested.startsWith("service-worker.js") ? `service-worker.js?v=${BUILD}` : url;
-      return originalRegister(nextUrl, options);
-    };
-  }
 })();
